c4b1a6773661bd576da20778fc4e3143cdf19c73
frozycar <autogelkor@proton.me>
2024-06-07 16:10:06 +0200

    Added tailscale install

--- config/script.sh
+++ config/script.sh
@@ -12,6 +12,16 @@ sudo apt upgrade
 sudo apt install wtype
 sudo raspi-config
 
+# Install Tailscale on Debian Bookworm
+curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+sudo apt-get update
+sudo apt-get install tailscale
+# Confirm in the browser the session
+sudo tailscale up
+# Check the IP
+tailscale ip -4
+
 # Install node
 sudo apt install curl -y 
 sudo apt install build-essential libssl-dev 
